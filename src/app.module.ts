cdeade1b44a3a362b07a6d7e8f74f88b47a38eea
Jordy Greenblatt <jordan.s.greenblatt@gmail.com>
2025-01-03 09:54:24 -0500

    Add websocket gateway for game session

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GameSessionGateway } from './game_session/game_session.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, GameSessionGateway],
 })
 export class AppModule {}
---
+++ src/game_session/game_session.gateway.ts
@@ -0,0 +1,15 @@
+// src/events/events.gateway.ts
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+} from '@nestjs/websockets';
+
+@WebSocketGateway()
+export class GameSessionGateway {
+  @SubscribeMessage('scene')
+  handleMessage(@MessageBody() data: string): string {
+    console.log(data);
+    return 'New scene';
+  }
+}
