4f6d36dd95688136fc07be1615de34786c109e48
Jordy Greenblatt <jordan.s.greenblatt@gmail.com>
2025-01-06 11:39:23 -0500

    Free port on shutdown

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(process.env.PORT ?? 3000);
+
+  // Graceful shutdown
+  process.on('SIGTERM', async () => {
+    console.log('SIGTERM signal received: closing HTTP server...');
+    await app.close();
+    console.log('HTTP server closed');
+  });
+
+  process.on('SIGINT', async () => {
+    console.log('SIGINT signal received: closing HTTP server...');
+    await app.close();
+    console.log('HTTP server closed');
+  });
 }
 bootstrap();
