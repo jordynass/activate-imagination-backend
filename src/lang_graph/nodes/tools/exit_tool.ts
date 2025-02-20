66676929d559986a981df1114c57079f5ca691c4
Jordy Greenblatt <jordan.s.greenblatt@gmail.com>
2025-02-20 10:44:50 -0500

    Modify exit tool prompt to avoid sending text.

--- src/lang_graph/nodes/tools/exit_tool.ts
+++ src/lang_graph/nodes/tools/exit_tool.ts
@@ -1,17 +1,23 @@
 import { tool } from '@langchain/core/tools';
 import { END } from '@langchain/langgraph';
+import { ExitEvent } from 'src/lang_graph/entities/output_events';
 import { OutputService } from 'src/shared/output.service';
 import { z } from 'zod';
 
 // TODO: Add exit confirmation step
 export function exitToolFactory(outputService: OutputService) {
   async function exitTool(_, config) {
-    outputService.sendEvent({ type: 'exit' }, config.configurable.thread_id); // Config thread_id matched gameId by design
+    outputService.sendEvent<void, ExitEvent>(
+      { type: 'exit' },
+      config.configurable.thread_id, // Config thread_id matched gameId by design
+    );
     return { next: END };
   }
   return tool(exitTool, {
     name: 'exit_game',
-    description: 'Exit the game when the user says they want to stop playing',
+    description:
+      'Exit the game when the user says they want to stop playing. \
+Do not include any text content when responding with this tool.',
     schema: z.object({}),
   });
 }
