9973af8745e72b683997ccdcf72f88c449343b00
Jordy Greenblatt <jordan.s.greenblatt@gmail.com>
2025-02-27 14:33:50 -0500

    Simplify game_master_node
    It really just goes to tools or the user action
    and could be a conditional edge instead
    of its own node. I don't think this is a good direction
    however, because it should update the game state in parallel
    based on the LLM's response. I would have to read
    about parallel nodes to know the best way to handle that.

--- src/lang_graph/nodes/game_master_node.ts
+++ src/lang_graph/nodes/game_master_node.ts
@@ -3,63 +3,29 @@
  * coordinating game flow
  */
 
-import { Command, END } from '@langchain/langgraph';
+import { Command } from '@langchain/langgraph';
 import { GraphAnnotation } from '../entities/state';
-import { type BaseMessage } from '@langchain/core/messages';
-import { InputKey } from 'src/shared/async_input.service';
 import { newLlm } from '../llm';
 
 export function gameMasterNodeFactory(tools) {
   async function gameMasterNode(
     state: typeof GraphAnnotation.State,
   ): Promise<Command> {
-    const latestMsg = state.messages[state.messages.length - 1];
-    const msgType = getMsgType(latestMsg);
-    switch (msgType) {
-      case MessageType.QUIT:
-        return new Command({ goto: END });
-      case MessageType.ACTION:
-        const nextMessage = await newLlm(tools).invoke(state.messages);
-        if (
-          'tool_calls' in nextMessage &&
-          Array.isArray(nextMessage.tool_calls) &&
-          nextMessage.tool_calls?.length
-        ) {
-          return new Command({
-            goto: 'toolNode',
-            update: { messages: [nextMessage] },
-          });
-        }
-        return new Command({
-          goto: 'heroActionNode',
-          update: { messages: [nextMessage] },
-        });
-      default:
-        console.error(
-          `Game Master Node does not recognize the most recent message on the main game thread:\n${JSON.stringify(latestMsg.toJSON(), null, 2)}`,
-        );
-        return new Command({ goto: END });
+    const nextMessage = await newLlm(tools).invoke(state.messages);
+    if (
+      'tool_calls' in nextMessage &&
+      Array.isArray(nextMessage.tool_calls) &&
+      nextMessage.tool_calls?.length
+    ) {
+      return new Command({
+        goto: 'toolNode',
+        update: { messages: [nextMessage] },
+      });
     }
+    return new Command({
+      goto: 'heroActionNode',
+      update: { messages: [nextMessage] },
+    });
   }
   return gameMasterNode;
-}
-
-enum MessageType {
-  ACTION,
-  NEW_SCENE,
-  QUIT,
-  UNKNOWN,
-}
-
-function getMsgType(msg: BaseMessage): MessageType {
-  if (typeof msg.content === 'string') {
-    // TODO: Make this an InputKey rather than text
-    if (['quit', 'q'].includes(msg.content.toLowerCase())) {
-      return MessageType.QUIT;
-    }
-    if (msg.additional_kwargs.inputKey === InputKey.ACTION) {
-      return MessageType.ACTION;
-    }
-  }
-  return MessageType.UNKNOWN;
 }
